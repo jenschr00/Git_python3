7b0658d79c1068a3f3542ae194e95014cb058645
Jens Christian Ásgrímsson <jensas278@nemi.tskoli.is>
2017-01-25 10:41:23 +0000

    daemi 1

--- pythonverkefni.py
+++ pythonverkefni.py
@@ -1,2 +1,8 @@
 #pythonverkefni
-
+print("Dæmi 1")
+tala1=int(input("sláðu inn tölu 1 "))
+tala2=int(input("sláðu inn tölu 2 "))
+summa=tala1+tala2
+margfoldun=tala1*tala2
+print("Summa talnanna er",summa)
+print("Margföldun talnanna er",margfoldun)
