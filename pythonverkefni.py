cdeb52fd33f0c0d7423ade20ace77c54b88121d9
Jens Christian Ásgrímsson <jensas278@nemi.tskoli.is>
2017-01-25 10:42:25 +0000

    daemi 3

--- pythonverkefni.py
+++ pythonverkefni.py
@@ -11,3 +11,21 @@ print("Dæmi 2")
 fornafn=input("Sláðu inn fornafn ")
 eftirnafn=input("Sláðu inn eftirnafn ")
 print("Halló ",fornafn,eftirnafn )
+
+print("Dæmi 3")
+hastafir=0
+lagstafir=0
+asdf=0
+asd=0
+texti=input("Sláðu inn texta ")
+for i in (texti):
+    if i.isupper():
+        hastafir+=1
+        if texti[asdf+1].islower():
+            asd+=1
+    if i.islower():
+        lagstafir+=1
+    asdf+=1
+print(hastafir,"Hástafir")
+print(lagstafir,"Lágstafir")
+print(asd,"Lágstafir strax á eftir hástöfum")
