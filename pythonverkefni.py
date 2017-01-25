cf19c191372606252b3f0d64b8dedfcdf87331ad
Jens Christian Ásgrímsson <jensas278@nemi.tskoli.is>
2017-01-25 10:41:53 +0000

    daemi 2

--- pythonverkefni.py
+++ pythonverkefni.py
@@ -6,3 +6,8 @@ summa=tala1+tala2
 margfoldun=tala1*tala2
 print("Summa talnanna er",summa)
 print("Margföldun talnanna er",margfoldun)
+
+print("Dæmi 2")
+fornafn=input("Sláðu inn fornafn ")
+eftirnafn=input("Sláðu inn eftirnafn ")
+print("Halló ",fornafn,eftirnafn )
